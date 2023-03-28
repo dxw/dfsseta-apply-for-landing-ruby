9ff133656ec35dd52e3a9134fe0208cd2bdfdaa0
Ed Davey <ed.davey@digital.justice.gov.uk>
2023-03-28 18:33:18 +0100

    Add 'Dates' form object to validate landing and departure dates
    - validates presence of both landing and departure dates
    - verifies that departure date is not before landing date
        (day trips are supported)
    - verifies that both dates are in the future. (All destinations
        are at least 24 hours journey time)

---
+++ app/forms/dates_form.rb
@@ -0,0 +1,26 @@
+class DatesForm
+  include ActiveModel::Model
+
+  attr_accessor :landing_date, :departure_date
+
+  def initialize(landing_date:, departure_date:)
+    @landing_date = landing_date
+    @departure_date = departure_date
+  end
+
+  validates :landing_date, presence: {message: "You must provide a landing date"}
+  validates :departure_date, presence: {message: "You must provide a departure date"}
+
+  validates_comparison_of :landing_date, greater_than: Date.today, message: "Landing date must be in the future"
+  validates_comparison_of :departure_date, greater_than: Date.today, message: "Departure date must be in the future"
+
+  validate :ensure_departure_not_before_landing
+
+  def ensure_departure_not_before_landing
+    return unless landing_date && departure_date
+
+    if landing_date > departure_date
+      errors.add(:base, "Landing date must be earlier than departure date")
+    end
+  end
+end
