d455f4d54d1d9b08f0ca2f372b5df83831922222
Ed Davey <ed@dxw.com>
2023-07-27 15:51:40 +0100

    Use answers repository in DestinationStageController to find/save answers

--- app/controllers/stages/destination_stage_controller.rb
+++ app/controllers/stages/destination_stage_controller.rb
@@ -4,19 +4,33 @@ class Stages::DestinationStageController < ApplicationController
   before_action :landable_bodies
 
   def show
-    @destination = DestinationForm.new(destination_id: "")
+    @destination = DestinationForm.new(
+      destination_id: answers.find(:destination)&.dig("destination_id")
+    )
   end
 
   def update
-    @destination = DestinationForm.new(destination_id: params[:destination_form][:destination_id])
+    @destination = DestinationForm.new(
+      destination_id: params[:destination_form][:destination_id]
+    )
+
     if @destination.valid?
+      answers.save(
+        stage_name: :destination, answer: {destination_id: @destination.destination_id}
+      )
       redirect_to(stages_dates_path)
     else
       render :show
     end
   end
 
+  private
+
   def landable_bodies
     @landable_bodies = LANDABLE_BODIES
+  end
+
+  def answers
+    @answers ||= AnswersRepository.new(session)
   end
 end
