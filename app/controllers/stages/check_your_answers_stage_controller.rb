af7f61fa888d55c46241d0c6b750fabd2ef1e5a3
Ed Davey <ed@dxw.com>
2023-11-02 17:02:51 +0000

    Render all summary list cards from answers stored in session
    In the controller we now build a CheckYourAnswers::Stage
    for every stage and populate it with the user's actual
    answers stored in the 'session'.
    
    We also extend the test fixtures to verify that each
    section with its questions and answers is being rendered as
    expected.
    
    This approach would need to be developed to cater for a long
    questionnaire, or one in which questions and answers are
    nested e.g.
    
    ```
    - section
      - task
        - page
          - questions
    ```
    
    But it seems like a reasonable level of abstraction for this
    service.

--- app/controllers/stages/check_your_answers_stage_controller.rb
+++ app/controllers/stages/check_your_answers_stage_controller.rb
@@ -2,6 +2,12 @@
 
 class Stages::CheckYourAnswersStageController < ApplicationController
   def show
+    stages_questions_and_answers
+  end
+
+  private
+
+  def stages_questions_and_answers
     @stages_questions_and_answers = [
       CheckYourAnswers::Stage.new(
         name: "destination",
@@ -12,10 +18,11 @@ class Stages::CheckYourAnswersStageController < ApplicationController
           CheckYourAnswers::Question.new(
             ref: :destination,
             title: "Destination",
-            answer: "Saturn (core)"
+            answer: destination_answer
           )
         ]
       ),
+
       CheckYourAnswers::Stage.new(
         name: "dates",
         title: "Dates",
@@ -25,15 +32,70 @@ class Stages::CheckYourAnswersStageController < ApplicationController
           CheckYourAnswers::Question.new(
             ref: :landing_date,
             title: "Requested landing date",
-            answer: "10 August 2024"
+            answer: date_answer("landing_date")
           ),
           CheckYourAnswers::Question.new(
             ref: :departure_date,
             title: "Requested departure date",
-            answer: "18 August 2024"
+            answer: date_answer("departure_date")
+          )
+        ]
+      ),
+
+      CheckYourAnswers::Stage.new(
+        name: "registration_identifier",
+        title: "Spacecraft Registration Identifier",
+        link_path: stages_registration_identifier_path,
+        link_text: "Registration ID",
+        questions: [
+          CheckYourAnswers::Question.new(
+            ref: :registration_id,
+            title: "Registration ID",
+            answer: answers.find(:registration_identifier)&.dig("registration_id")
+          )
+        ]
+      ),
+
+      CheckYourAnswers::Stage.new(
+        name: "personal_detals",
+        title: "Personal details",
+        link_path: stages_personal_details_path,
+        link_text: "personal details",
+        questions: [
+          CheckYourAnswers::Question.new(
+            ref: :fullname,
+            title: "Name",
+            answer: answers.find(:personal_details)&.dig("fullname")
+          ),
+          CheckYourAnswers::Question.new(
+            ref: :email,
+            title: "Email address",
+            answer: answers.find(:personal_details)&.dig("email")
+          ),
+          CheckYourAnswers::Question.new(
+            ref: :licence_id,
+            title: "Licence ID",
+            answer: answers.find(:personal_details)&.dig("licence_id")
           )
         ]
       )
     ]
+  end
+
+  def answers
+    @answers ||= AnswersRepository.new(session)
+  end
+
+  def destination_answer
+    LANDABLE_BODIES
+      .find { |body| body.id == answers.find(:destination)&.dig("destination_id") }
+      &.name
+  end
+
+  def date_answer(date_name)
+    Date.parse(answers.find(:dates)&.dig(date_name))
+      .strftime("%e %B %Y")
+  rescue TypeError, Date::Error
+    nil
   end
 end
