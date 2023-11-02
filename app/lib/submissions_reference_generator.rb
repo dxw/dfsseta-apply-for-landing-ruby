03883932eaea4a172c73d1da787166b35da4dfa2
Ed Davey <ed@dxw.com>
2023-11-02 23:20:34 +0000

    Generate random numeric and alphabetic elements of submissions references

--- app/lib/submissions_reference_generator.rb
+++ app/lib/submissions_reference_generator.rb
@@ -1,5 +1,15 @@
 class SubmissionsReferenceGenerator
   def self.generate
-    "AFL-321-CBA"
+    "AFL-#{random_numbers}-#{random_letters}"
+  end
+
+  private_class_method
+
+  def self.random_numbers
+    3.times.map { Kernel.rand(1..9).to_s }.join
+  end
+
+  def self.random_letters
+    3.times.map { ("A".."Z").to_a.sample }.join
   end
 end
