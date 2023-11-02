428d7910c8b0e47033d1276365fcad6a22afdaf9
Ed Davey <ed@dxw.com>
2023-11-02 16:36:30 +0000

    Extract markup to re-useable partials
    The expected content for the 2 stages which are being
    tested is still hard-coded, now in the controller.

--- app/controllers/stages/check_your_answers_stage_controller.rb
+++ app/controllers/stages/check_your_answers_stage_controller.rb
@@ -2,5 +2,38 @@
 
 class Stages::CheckYourAnswersStageController < ApplicationController
   def show
+    @stages_questions_and_answers = [
+      CheckYourAnswers::Stage.new(
+        name: "destination",
+        title: "Destination",
+        link_path: stages_destination_path,
+        link_text: "destination",
+        questions: [
+          CheckYourAnswers::Question.new(
+            ref: :destination,
+            title: "Destination",
+            answer: "Saturn (core)"
+          )
+        ]
+      ),
+      CheckYourAnswers::Stage.new(
+        name: "dates",
+        title: "Dates",
+        link_path: stages_dates_path,
+        link_text: "dates",
+        questions: [
+          CheckYourAnswers::Question.new(
+            ref: :landing_date,
+            title: "Requested landing date",
+            answer: "10 August 2024"
+          ),
+          CheckYourAnswers::Question.new(
+            ref: :departure_date,
+            title: "Requested departure date",
+            answer: "18 August 2024"
+          )
+        ]
+      )
+    ]
   end
 end
