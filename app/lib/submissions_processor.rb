95f07a250a4eac2fb87f8d2746a974e9852fe06b
Ed Davey <ed@dxw.com>
2024-08-21 16:20:51 +0100

    Refactor SubmissionProcessor for clarity
    We group the processing of answers to make them
    easier to review. According to steps in
    the multi-stage wizard, and some other categories.

--- app/lib/submissions_processor.rb
+++ app/lib/submissions_processor.rb
@@ -7,32 +7,59 @@ class SubmissionsProcessor
 
   def call
     @repository_class.create(
-      destination: destination,
-      pilot_name: @session.dig("personal_details", "fullname"),
-      pilot_email: @session.dig("personal_details", "email"),
-      pilot_licence_id: @session.dig("personal_details", "licence_id"),
-      spacecraft_registration_id: @session.dig("registration_identifier", "registration_id"),
-      landing_date: landing_date,
-      departure_date: departure_date,
-      application_reference: @reference,
-      application_submitted_at: Time.now,
-      application_decision: nil,
-      application_decision_made_at: nil,
-      permit_id: nil
+      destination
+        .merge(reference)
+        .merge(dates)
+        .merge(registration_identifier)
+        .merge(personal_details)
+        .merge(submitted_at)
+        .merge(blank_fields)
     )
   end
 
   private
 
   def destination
-    LandableBody.find(@session.dig("destination", "destination_id"))
+    {destination: LandableBody.find(@session.dig("destination", "destination_id"))}
   end
 
-  def landing_date
-    Date.parse(@session.dig("dates", "landing_date"))
+  def reference
+    {application_reference: @reference}
   end
 
-  def departure_date
-    Date.parse(@session.dig("dates", "departure_date"))
+  def dates
+    {
+      landing_date: Date.parse(@session.dig("dates", "landing_date")),
+      departure_date: Date.parse(@session.dig("dates", "departure_date"))
+    }
+  end
+
+  def registration_identifier
+    {
+      spacecraft_registration_id: @session.dig(
+        "registration_identifier",
+        "registration_id"
+      )
+    }
+  end
+
+  def personal_details
+    {
+      pilot_name: @session.dig("personal_details", "fullname"),
+      pilot_email: @session.dig("personal_details", "email"),
+      pilot_licence_id: @session.dig("personal_details", "licence_id")
+    }
+  end
+
+  def submitted_at
+    {application_submitted_at: Time.now}
+  end
+
+  def blank_fields
+    {
+      application_decision: nil,
+      application_decision_made_at: nil,
+      permit_id: nil
+    }
   end
 end
