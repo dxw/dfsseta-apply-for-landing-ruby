7e8c68ed110c770f4ff279dc7e28175bd842aa81
Patrick Fleming <patrick.fleming@dxw.com>
2024-09-10 11:20:12 +0100

    Update LandingApplication model to include belongs_to association to User model
    We must specify `optional: true` because our landing applications will only have an assessor association once they have been assessed.

--- app/models/landing_application.rb
+++ app/models/landing_application.rb
@@ -1,3 +1,5 @@
 class LandingApplication < ApplicationRecord
   belongs_to :destination, foreign_key: :destination_id, class_name: "LandableBody"
+
+  belongs_to :assessor, foreign_key: :assessor_id, class_name: "User", optional: true
 end
