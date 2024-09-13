8b1e2163607d5b5080daf35173ae65a4b2fcac30
Patrick Fleming <patrick.fleming@dxw.com>
2024-09-13 16:15:27 +0100

    Convert application_decision field to enum
    This gives us additional query methods on the model: https://apidock.com/rails/ActiveRecord/Enum

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.2].define(version: 2024_09_09_125011) do
+ActiveRecord::Schema[7.2].define(version: 2024_09_13_140449) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -31,12 +31,12 @@ ActiveRecord::Schema[7.2].define(version: 2024_09_09_125011) do
     t.date "departure_date", null: false
     t.string "application_reference", null: false
     t.datetime "application_submitted_at", null: false
-    t.string "application_decision"
     t.datetime "application_decision_made_at"
     t.string "permit_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.uuid "assessor_id"
+    t.integer "application_decision"
     t.index ["assessor_id"], name: "index_landing_applications_on_assessor_id"
     t.index ["destination_id"], name: "index_landing_applications_on_destination_id"
   end
